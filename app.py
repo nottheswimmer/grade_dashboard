e54372f70fae86deac240fca450687a4037e5378
Michael Phelps <michael.phelps@cpcc.edu>
2020-12-01 18:32:55 -0500

    Add some kind of visualization

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+import plotly.express as px
+from pandas import DataFrame as df
 from canvasapi import Canvas
 from flask import Flask, request, session, render_template, redirect, url_for
+from plotly.io import to_html
 
 app = Flask(__name__)
 app.config.update(
@@ -55,6 +58,7 @@ def show_grade(course_id):
     canvas = get_canvas()
     course = canvas.get_course(course_id)
     current_user = canvas.get_current_user()
+    submission_data = []
     groups = {}
     for group in course.get_assignment_groups():
         groups[group.id] = (group, [])
@@ -70,7 +74,27 @@ def show_grade(course_id):
             submission = assignment.get_submission(user=current_user)
             # score = submission.score
         groups[assignment.assignment_group_id][1].append((assignment, submission))
-    return render_template('course.html', name=session[CANVAS_NAME_PARAM], course=course, groups=groups)
+
+    for group, assignments_and_submissions in groups.values():
+        if group.group_weight and group.group_weight > 0:
+            for assignment, submission in assignments_and_submissions:
+                submission_score = "N/A"
+                submission_graded_at_date = "ungraded"
+                assignment_points_possible = "N/A"
+                if submission:
+                    if getattr(submission, 'score', None) is not None:
+                        submission_score = submission.score
+                    if getattr(submission, 'graded_at_date', None) is not None:
+                        submission_graded_at_date = submission.graded_at_date
+                if assignment:
+                    if getattr(assignment, 'points_possible', None) is not None:
+                        assignment_points_possible = assignment.points_possible
+                submission_data.append([str(x) for x in [group, group.group_weight, assignment, submission_score, assignment_points_possible, submission_graded_at_date]])
+    submission_data = df(submission_data, columns=["group", "group_weight", "assignment", "submission_score", "assignment_points_possible", 'submission_graded_at_date'])
+    submission_data['count'] = submission_data.groupby('group')['group'].transform('count')
+    fig = px.bar(submission_data, x='group', y='assignment')
+    html = to_html(fig).split("<body>", 1)[-1].split("</body>", 1)[0]
+    return render_template('course.html', name=session[CANVAS_NAME_PARAM], course=course, html=html)
 
 
 if __name__ == '__main__':
